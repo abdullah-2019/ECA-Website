a112db4847a67f4283c10f7525aa275ba7e4f46e
abdullah-2019 <h.abdullah1394@gmail.com>
2024-05-16 17:42:01 +0430

    Update header.blade.php
    header updated

--- resources/views/site/layouts/header.blade.php
+++ resources/views/site/layouts/header.blade.php
@@ -42,7 +42,7 @@
                 </li>
                 <li><a href="{{ route('sector.index') }}"
                         class="{{ request()->is('sector*') ? 'active' : '' }}">Sectors</a></li>
-                <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+                {{-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
                         <li><a href="#">Drop Down 1</a></li>
                         <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i
@@ -59,7 +59,7 @@
                         <li><a href="#">Drop Down 3</a></li>
                         <li><a href="#">Drop Down 4</a></li>
                     </ul>
-                </li>
+                </li> --}}
                 <li><a href="{{ route('contactus.index') }}"
                         class="{{ request()->is('contactus*') ? 'active' : '' }} ">Contact Us</a></li>
             </ul>

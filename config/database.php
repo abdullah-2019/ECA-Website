11cabd73413f524977ca49c17ec224d16a6c8701
abdullah-2019 <h.abdullah1394@gmail.com>
2024-06-10 19:16:59 +0430

    gitignore update
    gitignore updated

--- config/database.php
+++ config/database.php
@@ -34,7 +34,8 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            // 'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => dirname(__DIR__) . '/database/database.sqlite',
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
@@ -144,7 +145,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
